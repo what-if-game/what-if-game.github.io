496d9f290268f508908bf66ad2795d76704ec0ce
Anson Huang <ansonhuang1216@gmail.com>
2024-03-14 15:44:17 -0400

    update

--- game/main.js
+++ game/main.js
@@ -1,40 +1,9 @@
 (function(storyContent) {
-    var storyString = `LONDON, 1872
-Residence of Monsieur Phileas Fogg.
--> london
-
-=== london ===
-Monsieur Phileas Fogg returned home early from the Reform Club, and in a new-fangled steam-carriage, besides!  
-"Passepartout," said he. "We are going around the world!"
-
-+ "Around the world, Monsieur?"
-    I was utterly astonished. 
-    -> astonished
-+ [Nod curtly.] -> nod
-
-
-=== astonished ===
-"You are in jest!" I told him in dignified affront. "You make mock of me, Monsieur."
-"I am quite serious."
-
-+ "But of course"
-    -> ending
-
-
-=== nod ===
-I nodded curtly, not believing a word of it.
--> ending
-
-
-=== ending
-"We shall circumnavigate the globe within eighty days." He was quite calm as he proposed this wild scheme. "We leave for Paris on the 8:25. In an hour."
--> END`;
-    const story = new inkjs.Compiler(storyString).Compile();
+    const story = new inkjs.Compiler(storyContent).Compile();
     // story is an inkjs.Story that can be played right away
     
     // const jsonBytecode = story.ToJson();
     // // the generated json can be further re-used
-    // var story = new inkjs.Story(storyContent);
 
     var storyContainer = document.querySelectorAll('#story')[0];
 
