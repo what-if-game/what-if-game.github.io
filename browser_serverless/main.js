2cb80e4d2dd04a93d37692c04308102f0a8d808b
Anson Huang <ansonhuang1216@gmail.com>
2024-03-14 11:01:29 -0400

    Update main.js

--- browser_serverless/main.js
+++ browser_serverless/main.js
@@ -1,10 +1,11 @@
+var Story = require('inkjs').Compiler;
 (function(storyContent) {
     var storyString = `=== find_help ===
 
 	You search desperately for a friendly face in the crowd.
 	*	The woman in the hat[?] pushes you roughly aside. -> find_help
 	*	The man with the briefcase[?] looks disgusted as you stumble past him. -> find_help`;
-    const story = new inkjs.Compiler(storyString).Compile();
+    const story = new Compiler(storyString).Compile();
     // story is an inkjs.Story that can be played right away
     
     // const jsonBytecode = story.ToJson();
